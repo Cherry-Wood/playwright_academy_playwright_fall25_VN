0bc3d60718aa47cd4456afec866490ea99129845
Cherry-Wood <CherryWood@gmx.com>
2025-11-17 17:22:18 +0100

    fluent lekce

--- src/pages/pmtool/lost_password_page.ts
+++ src/pages/pmtool/lost_password_page.ts
@@ -1,10 +1,12 @@
 import { Locator, Page } from "@playwright/test";
+import { LoginPage } from "./login_page.ts";
 
 export class LostPasswordPage {
   readonly page: Page;
   readonly emailInput: Locator;
   readonly usernameInput: Locator;
   readonly sentButton: Locator;
+  readonly backButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -15,17 +17,26 @@ export class LostPasswordPage {
       ":nth-child(2) > .input-icon > .form-control"
     );
     this.sentButton = page.locator(".btn-info");
+    this.backButton = page.locator("#back-btn");
   }
 
   async clickSent() {
     await this.sentButton.click();
+    return new LoginPage(this.page);
   }
 
   async fillEmail(email: string) {
     await this.emailInput.fill(email);
+    return this;
   }
 
   async fillUsername(username: string) {
     await this.usernameInput.fill(username);
+    return this;
+  }
+
+  async clickBack() {
+    await this.backButton.click();
+    return new LoginPage(this.page);
   }
 }
